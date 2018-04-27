1655d09a0c0084efeace4f9deb9ca0d6ba754900
Croff <324716891@qq.com>
2018-04-27 12:26:23 +0800

    添加使用说明

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # ImagePuzzleGame
-一个JavaFX做的拼图小游戏，使用BFS计算路径。
+一个JavaFX做的拼图小游戏，使用BFS计算解决路径。
+
+## 运行方法
+> cd ImagePuzzleGame
+
+> mvn install
+
+> cd target
+
+> java -jar ImagePuzzleGame.jar
+
+## 使用说明
+* open按钮可以打开任意图片进行游戏
+* reset按钮可以重置游戏
+* solution按钮可以使用BFS搜索出解决方案并演示
--- src/main/java/cn/croff/Main.java
+++ src/main/java/cn/croff/Main.java
@@ -5,7 +5,6 @@ import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
-import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -30,15 +29,105 @@ import java.util.Vector;
 
 public class Main extends Application {
 
-    public int missing_piece; // missing_piece represents the position where has no image there or blank area
-    int[] number;// = {0, 1, 2, 3, 5, 4, 6, 7, 8};
-    int complexity = 30;
+    private int missing_piece;
+    // the position where has no image there or blank area
+    private int[] number;
+    // number = {0, 1, 2, 3, 5, 4, 6, 7, 8};
+    private int complexity = 30;
+    private ImageView[] imageViews = new ImageView[9];
+    private String fileName;
+    private RandomGenerator randgen;
+    // the random puzzle patten generator
 
-    RandomGenerator randgen; //the random puzzle patten generator
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage arg0) {
+        init(arg0);
+    }
+
+    public void init(Stage stage) {
+        randgen = new RandomGenerator();
+        number = new int[9];
+        missing_piece = randgen.getRandomSeq(number, complexity); //generate missing number
+
+
+        fileName = "image/image.jpg";
+        Image image = new Image(fileName);
+        GridPane gridPane = new GridPane();
+        for (int i = 0, k = 0; i <= 2; ++i) {
+            for (int j = 0; j <= 2; ++j, ++k) {
+                imageViews[k] = new ImageView(image);
+                // initial the array
+                imageViews[k].setOnMouseClicked(new myevent());
+                // set mouse click function
+                imageViews[k].setViewport(new Rectangle2D(100 * j, 100 * i, 100, 100));
+                // cut the image to 9 pieces
+            }
+        }
+
+        gridPane.add(imageViews[number[0]], 0, 0);
+        // add imageviews to grid pane as same as random sequence
+        gridPane.add(imageViews[number[1]], 1, 0);
+        gridPane.add(imageViews[number[2]], 2, 0);
+        gridPane.add(imageViews[number[3]], 0, 1);
+        gridPane.add(imageViews[number[4]], 1, 1);
+        gridPane.add(imageViews[number[5]], 2, 1);
+        gridPane.add(imageViews[number[6]], 0, 2);
+        gridPane.add(imageViews[number[7]], 1, 2);
+        gridPane.add(imageViews[number[8]], 2, 2);
+
+        ImageView comp = new ImageView(image);
+        comp.setViewport(new Rectangle2D(0, 0, 300, 300));
+        Image image2 = new Image("image/white.png");
+        imageViews[missing_piece].setImage(image2);// use to set white image
+        gridPane.setGridLinesVisible(true);
+        BorderPane borderPane = new BorderPane(gridPane);
+        VBox right = new VBox(20, comp);
+        HBox buttons = new HBox(50);
+        buttons.setPadding(new Insets(20));
+        Button open = new Button("open");// a button to set another image
+        final FileChooser fileChooser = new FileChooser();
+        open.setOnAction(event -> {
+            File file = fileChooser.showOpenDialog(stage);
+            if (file != null) {
+                fileName = file.getAbsolutePath();
+                resetStage(stage);
+                System.out.print(fileName);
+            }
+        });
+        Button reset = new Button("reset");
+        reset.setOnAction(event -> init(stage));
+        Button solution = new Button("solution");
+        solution.setOnAction(event1 -> {
+            Vector solution_path = Solution.BFS(number, missing_piece);
+
+            if (solution_path.size() > 0) {
+                final IntegerProperty i = new SimpleIntegerProperty(0);
+                Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(1), event -> {
+                    i.set(i.get() + 1);
+                    swapNums(number[(int) solution_path.get(i.get() - 1)], number[(int) solution_path.get(i.get())]);
+                }));
+                timeline.setCycleCount(solution_path.size() - 1);
+                timeline.play();
+            }
+        });
+        buttons.getChildren().addAll(open, reset, solution);
+        borderPane.setRight(right);
+        borderPane.setBottom(buttons);
+        buttons.setAlignment(Pos.CENTER);
+        Scene scene = new Scene(borderPane, 650, 400);
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+
+    }
 
     /* This function will swap two numbers at the positions first_num and second_num: a[first_num], b[second_num]*/
-    public void swapNums(int first_num, int second_num) // swap two images
-    {
+    public void swapNums(int first_num, int second_num) {
+        // swap two images
         int row1 = GridPane.getRowIndex(imageViews[first_num]);
         int colu1 = GridPane.getColumnIndex(imageViews[first_num]);
         int row2 = GridPane.getRowIndex(imageViews[second_num]);
@@ -70,117 +159,10 @@ public class Main extends Application {
         System.out.println("******");
     }
 
-    public ImageView emptimg = new ImageView();
-    ImageView[] imageViews = new ImageView[9];
-
-    private String fileName;
-
-    public static void main(String[] args) {
-        Application.launch(args);
-    }
-
-    @Override
-    public void start(Stage arg0) throws Exception {
-        init(arg0);
-    }
-
-    public void init(Stage stage) {
-        randgen = new RandomGenerator();
-        number = new int[9];
-        missing_piece = randgen.getRandomSeq(number, complexity); //generate missing number
-
-
-        fileName = "image/image.jpg";
-        Image image = new Image(fileName);
-        GridPane gridPane = new GridPane();
-        for (int i = 0, k = 0; i <= 2; ++i) {
-            for (int j = 0; j <= 2; ++j, ++k) {
-                imageViews[k] = new ImageView(image);// initial the array
-                imageViews[k].setOnMouseClicked(new myevent());// set mouse
-                // click
-                // function
-                imageViews[k].setViewport(new Rectangle2D(100 * j, 100 * i, 100, 100));// cut
-                // the
-                // image
-                // to
-                // 9
-                // pieces
-            }
-        }
-
-        gridPane.add(imageViews[number[0]], 0, 0);// add imageviews to grid pane
-        // as same as random
-        // sequence
-        gridPane.add(imageViews[number[1]], 1, 0);
-        gridPane.add(imageViews[number[2]], 2, 0);
-        gridPane.add(imageViews[number[3]], 0, 1);
-        gridPane.add(imageViews[number[4]], 1, 1);
-        gridPane.add(imageViews[number[5]], 2, 1);
-        gridPane.add(imageViews[number[6]], 0, 2);
-        gridPane.add(imageViews[number[7]], 1, 2);
-        gridPane.add(imageViews[number[8]], 2, 2);
-
-        ImageView comp = new ImageView(image);
-        comp.setViewport(new Rectangle2D(0, 0, 300, 300));
-        Image image2 = new Image("image/white.png");
-        imageViews[missing_piece].setImage(image2);// use to set white image
-        gridPane.setGridLinesVisible(true);
-        BorderPane borderPane = new BorderPane(gridPane);
-        VBox right = new VBox(20, comp);
-        HBox buttons = new HBox(50);
-        buttons.setPadding(new Insets(20));
-        Button open = new Button("open");// a button to set another image
-        final FileChooser fileChooser = new FileChooser();
-        open.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                File file = fileChooser.showOpenDialog(stage);
-                if (file != null) {
-                    fileName = file.getAbsolutePath();
-                    resetStage(stage);
-                    System.out.print(fileName);
-                }
-            }
-        });
-        Button reset = new Button("reset");
-        reset.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                init(stage);
-            }
-        });
-        Button solution = new Button("solution");
-        solution.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event1) {
-                Vector solution_path = Solution.BFS(number, missing_piece);
-
-                if (solution_path.size() > 0) {
-                    final IntegerProperty i = new SimpleIntegerProperty(0);
-                    Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(1), event -> {
-                        i.set(i.get() + 1);
-                        swapNums(number[(int) solution_path.get(i.get() - 1)], number[(int) solution_path.get(i.get())]);
-                    }));
-                    timeline.setCycleCount(solution_path.size() - 1);
-                    timeline.play();
-                }
-            }
-        });
-        buttons.getChildren().addAll(open, reset, solution);
-        borderPane.setRight(right);
-        borderPane.setBottom(buttons);
-        buttons.setAlignment(Pos.CENTER);
-        Scene scene = new Scene(borderPane, 650, 400);
-        stage.setScene(scene);
-        stage.setResizable(false);
-        stage.show();
-
-    }
-
-    public void resetStage(Stage stage) // use to restart current image
-    {
-
-        missing_piece = randgen.getRandomSeq(number, complexity); //generate missing number
+    public void resetStage(Stage stage) {
+        // use to restart current image
+        missing_piece = randgen.getRandomSeq(number, complexity);
+        //generate missing number
         Image image = new Image("file:///" + fileName);// for mac os
         // Image image = new Image("file://"+fileName);//for windows os
         GridPane gridPane = new GridPane();
@@ -213,15 +195,12 @@ public class Main extends Application {
         buttons.setPadding(new Insets(20));
         Button open = new Button("open");
         final FileChooser fileChooser = new FileChooser();
-        open.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                File file = fileChooser.showOpenDialog(stage);
-                if (file != null) {
-                    fileName = file.getAbsolutePath();
-                    resetStage(stage);
-                    System.out.print(fileName);
-                }
+        open.setOnAction(event -> {
+            File file = fileChooser.showOpenDialog(stage);
+            if (file != null) {
+                fileName = file.getAbsolutePath();
+                resetStage(stage);
+                System.out.print(fileName);
             }
         });
 
@@ -249,13 +228,11 @@ public class Main extends Application {
         stage.setScene(scene);
         stage.setResizable(false);
         stage.show();
-
     }
 
 
-    class myevent implements EventHandler<MouseEvent> // implement click
-            // function
-    {
+    class myevent implements EventHandler<MouseEvent> {
+        // implement click function
 
         @Override
         public void handle(MouseEvent arg0) {
@@ -270,34 +247,30 @@ public class Main extends Application {
             double sy = img.getLayoutY();
             double dispx = sx - imageViews[missing_piece].getLayoutX();
             double dispy = sy - imageViews[missing_piece].getLayoutY();
-            if ((dispx == -100) && (dispy == 0)) // click the left image of
-            // white image
-            {
-                swapNums(number[idx], number[midx]);// swap two images
-                if (issucc(imageViews)) // check successful
-                {
+            if ((dispx == -100) && (dispy == 0)) {
+                // click the left image of white image
+                swapNums(number[idx], number[midx]);
+                // swap two images
+                if (issucc(imageViews)) {
                     Alert alert = new Alert(AlertType.WARNING, "Success！");
                     alert.show();
                 }
-            } else if ((dispx == 0) && (dispy == -100)) // click the top image of
-            // white image
-            {
+            } else if ((dispx == 0) && (dispy == -100)) {
+                // click the top image of white image
                 swapNums(number[idx], number[midx]);
                 if (issucc(imageViews)) {
                     Alert alert = new Alert(AlertType.WARNING, "Success！");
                     alert.show();
                 }
-            } else if ((dispx == 100) && (dispy == 0)) // click the right image
-            // of white image
-            {
+            } else if ((dispx == 100) && (dispy == 0)) {
+                // click the right image of white image
                 swapNums(number[idx], number[midx]);
                 if (issucc(imageViews)) {
                     Alert alert = new Alert(AlertType.WARNING, "Success！");
                     alert.show();
                 }
-            } else if ((dispx == 0) && (dispy == 100)) // click the bottom image
-            // of white image
-            {
+            } else if ((dispx == 0) && (dispy == 100)) {
+                // click the bottom image of white image
                 swapNums(number[idx], number[midx]);
                 if (issucc(imageViews)) {
                     Alert alert = new Alert(AlertType.WARNING, "Success！");
